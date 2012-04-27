53437e9a4214edffae4625f338d4c1566a6aaeb0
akamk87 <akamk87@24ab77ea-4c93-5923-add9-110dc26b9814>
2012-04-27 14:12:19 +0000

    Tab skeleton code

---
+++ trunk/DontKoala/src/kaistcs/android/dontkoala/GroupTab.java
@@ -0,0 +1,17 @@
+package kaistcs.android.dontkoala;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GroupTab extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+        setContentView(R.layout.group);
+	}
+
+}
