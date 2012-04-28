4a3f1776cb0d969ca8a8062c808bed41501355cb
extratype@gmail.com <extratype@gmail.com@24ab77ea-4c93-5923-add9-110dc26b9814>
2012-04-28 14:45:39 +0000

    Change tab icon

--- DontKoala/src/kaistcs/android/dontkoala/DontKoalaActivity.java
+++ DontKoala/src/kaistcs/android/dontkoala/DontKoalaActivity.java
@@ -21,9 +21,15 @@ public class DontKoalaActivity extends TabActivity implements OnTabChangeListene
         mTabHost = getTabHost();
         
         // 일단 탭 아이콘이 프로필 밖에 없어서 전부 통일, 나중에 이미지만 변경
-        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Profile", getResources().getDrawable(R.drawable.profile)).setContent(new Intent(this, ProfileTab.class)));
-        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("Group", getResources().getDrawable(R.drawable.profile)).setContent(new Intent(this, GroupTab.class)));
-        mTabHost.addTab(mTabHost.newTabSpec("tab3").setIndicator("Notification", getResources().getDrawable(R.drawable.profile)).setContent(new Intent(this, NotificationTab.class)));
+        mTabHost.addTab(mTabHost.newTabSpec("profile")
+        						.setIndicator("Profile", getResources().getDrawable(R.drawable.ic_tab_profile))
+        						.setContent(new Intent(this, ProfileTab.class)));
+        mTabHost.addTab(mTabHost.newTabSpec("group")
+        						.setIndicator("Group", getResources().getDrawable(R.drawable.ic_tab_group))
+        						.setContent(new Intent(this, GroupTab.class)));
+        mTabHost.addTab(mTabHost.newTabSpec("notification")
+        						.setIndicator("Notifications", getResources().getDrawable(R.drawable.ic_tab_notification))
+        						.setContent(new Intent(this, NotificationTab.class)));
         
         mTabHost.setOnTabChangedListener(this);
         mTabHost.setCurrentTab(0);
@@ -32,11 +38,11 @@ public class DontKoalaActivity extends TabActivity implements OnTabChangeListene
 	@Override
 	public void onTabChanged(String tabId) {
 		// 탭 체인지될 경우 해당 탭의 액티비티 호출과 상관없이 메인에서 처리할 부분 (딱히 없으면 그냥 주석)
-    	if (tabId.equals("tab1")) {
+    	if (tabId.equals("profile")) {
     		//Toast.makeText(this, "Profile tab", Toast.LENGTH_SHORT).show();
-    	} else if (tabId.equals("tab2")) {
+    	} else if (tabId.equals("group")) {
     		//Toast.makeText(this, "Group tab", Toast.LENGTH_SHORT).show();
-    	} else if (tabId.equals("tab3")) {
+    	} else if (tabId.equals("notification")) {
     		//Toast.makeText(this, "Notification tab", Toast.LENGTH_SHORT).show();
     	} else {
     		//
