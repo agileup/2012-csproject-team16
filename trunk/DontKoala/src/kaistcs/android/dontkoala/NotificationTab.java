7cffcfc319b45f745e34978999e52e70b5c17502
akamk87 <akamk87@24ab77ea-4c93-5923-add9-110dc26b9814>
2012-05-10 05:54:49 +0000

    그룹 탭 구현 완료 아직 프로필탭에서 이름가져오는건 못함 (지금은 임의로 그냥 직접 스트링 보냄)
    이제 푸쉬 기능 구현

--- trunk/DontKoala/src/kaistcs/android/dontkoala/NotificationTab.java
+++ trunk/DontKoala/src/kaistcs/android/dontkoala/NotificationTab.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class NotificationTab extends Activity {
-
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -12,6 +12,7 @@ public class NotificationTab extends Activity {
 	
 	    // TODO Auto-generated method stub
         setContentView(R.layout.notification);
+
 	}
 
 }
