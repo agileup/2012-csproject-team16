c70c5161d6fe743e9e33d206722a54f33a3cc0d4
extratype@gmail.com <extratype@gmail.com@24ab77ea-4c93-5923-add9-110dc26b9814>
2012-05-13 06:07:07 +0000

    Completed: Home Location

--- trunk/DontKoala/src/kaistcs/android/dontkoala/ProfileTab.java
+++ trunk/DontKoala/src/kaistcs/android/dontkoala/ProfileTab.java
@@ -1,8 +1,9 @@
 package kaistcs.android.dontkoala;
 
+import java.text.ParseException;
+
 import android.os.Bundle;
 import android.preference.EditTextPreference;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.Preference;
 import android.preference.CheckBoxPreference;
@@ -11,13 +12,56 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 
 public class ProfileTab extends PreferenceActivity {
-	private EditTextPreference mName;
-	private Preference mHomeLocation;
-	private Preference mEmergencyContacts;
-	private CheckBoxPreference mSendLocation;
-	private EditTextPreference mPresetText;
+	EditTextPreference mName;
+	Preference mHomeLocation;
+	Preference mEmergencyContacts;
+	CheckBoxPreference mSendLocation;
+	EditTextPreference mPresetText;
 	
 	private SharedPreferences sharedPrefs;
+	private static final int REQ_HOME_LOCATION = 0;
+	
+	// helper class for reading/writing HomeLocation preference
+	public static class HomeLocationPref {
+		private int latE6;
+		private int longE6;
+		private String addr = "";
+		
+		public HomeLocationPref(int latE6, int longE6, String addr) {
+			this.latE6 = latE6;
+			this.longE6 = longE6;
+			this.addr = addr;
+		}
+		
+		public HomeLocationPref(String prefString) throws ParseException, NumberFormatException{
+			String[] parts = prefString.split(",");
+			
+			if (parts.length == 3) {
+				latE6 = Integer.parseInt(parts[0]);
+				longE6 = Integer.parseInt(parts[1]);
+				addr = parts[2];
+			} else {
+				throw new ParseException(prefString, prefString.length());
+			}
+		}
+		
+		@Override
+		public String toString() {
+			return latE6 + "," + longE6 + "," + addr;
+		}
+		
+		public int getLatitudeE6() {
+			return latE6;
+		}
+		
+		public int getLongitudeE6() {
+			return longE6;
+		}
+		
+		public String getAddress() {
+			return addr;
+		}
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,12 +75,22 @@ public class ProfileTab extends PreferenceActivity {
 		mPresetText = (EditTextPreference)findPreference("profile_preset_text");
 		
 		mEmergencyContacts.setIntent(new Intent(this, EmergencyContacts.class));
-		mHomeLocation.setIntent(new Intent(this, HomeLocation.class));
 		
 		mName.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
 			@Override
 			public boolean onPreferenceChange(Preference preference, Object newValue) {
 				mName.setSummary((String) newValue);
+				return true;
+			}
+		});
+		
+		mHomeLocation.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Intent i = new Intent(ProfileTab.this, HomeLocation.class);
+				i.putExtra("HomeLocationPref", sharedPrefs.getString(mHomeLocation.getKey(), ""));
+				startActivityForResult(i, REQ_HOME_LOCATION);
+				
 				return true;
 			}
 		});
@@ -48,6 +102,25 @@ public class ProfileTab extends PreferenceActivity {
 	public void onResume() {
 		super.onResume();
 		mName.setSummary(mName.getText());
-		mHomeLocation.setSummary(sharedPrefs.getString(mHomeLocation.getKey(), ""));	// TODO: display address instead of "latitude,longitude". 
+		
+		try {
+			HomeLocationPref parser = new HomeLocationPref(sharedPrefs.getString(mHomeLocation.getKey(), ""));
+			
+			// If address is not available, then show lat,long
+			if (parser.getAddress().isEmpty() == true)
+				mHomeLocation.setSummary( ((double)parser.getLatitudeE6()/1E6) + ", " + ((double)parser.getLongitudeE6()/1E6) );
+			else
+				mHomeLocation.setSummary(parser.getAddress());
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQ_HOME_LOCATION && resultCode == RESULT_OK) {
+			String prefString = data.getStringExtra("HomeLocationPref");
+			sharedPrefs.edit().putString(mHomeLocation.getKey(), prefString).apply();
+		}
 	}
 }
